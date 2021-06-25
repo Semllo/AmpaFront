8edba8412445ff04ca4f67e2e843c7a4328aa416
unknown <marcsemperlloret@gmail.com>
2021-06-25 19:18:47 +0200

    final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   base_url: 'https://backend.ampaiesjaumeprimer.es/api'
-
 };
 // http://localhost:3000/api
 //https://backend.ampaiesjaumeprimer.es/api
